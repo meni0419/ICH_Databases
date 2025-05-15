b4415ed0b6064ed985ff3510766678cc13b33a22
meni0419 <meni0419@gmail.com>
2025-05-15 13:19:51 +0300

    pr56

--- MongoDB/Practice/pr56.py
+++ MongoDB/Practice/pr56.py
@@ -1,13 +1,34 @@
 from MongoDB.collection import collections
 
 queries = {
-    # Найти среднее количество городов в штате
-    "pr56_task1": lambda: collections.sample_training
+    "pr56_task1": lambda: collections.UK_used_cars
+    .find_one({}),
+
+    # Выберите документы, в которых общественный транспорт был быстрее автомобиля.
+    "pr56_task2": lambda: collections.bookings
+    .count_documents({
+        "$expr": {
+            "$lt": ["$public_transport.duration", "$driving.duration"]
+        }
+    }),
+
+    # add fields
+    "pr56_task3": lambda: collections.bookings
     .aggregate([
-        {'$group':
-             {'_id': '$state',
-              'count_cities': {'$sum': 1}}},
-        {'$sort': {'count_cities': -1}},
-        {'$limit': 1}
-    ]),
+    {
+        '$addFields': {
+            'total_distance': {
+                '$add': [
+                    '$driving.distance', '$walking.distance', '$public_transport.distance'
+                ]
+            },
+            'avg_distance': {
+                '$avg': [
+                    '$driving.distance', '$walking.distance', '$public_transport.distance'
+                ]
+            }
+        }
+    },
+        {"$limit": 10}
+])
 }
